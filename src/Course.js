19d6a28b86338bce8fb705e0986ddd71603907f4
canberk164 <canberk164@gmail.com>
2024-01-30 20:13:57 +0300

    adding codes

---
+++ src/Course.js
@@ -0,0 +1,10 @@
+import Angular from "./images/angular.jpg";
+import Bootstrap from "./images/bootstrap5.png";
+import Ccsharp from "./images/ccsharp.png";
+import Web from "./images/web.jpg";
+
+function Course() {
+  return <div>asdasdasd</div>;
+}
+
+export default Course;
