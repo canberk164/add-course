af28f4720a5563b2a8aedd7fcef704023f6a0874
canberk164 <canberk164@gmail.com>
2024-01-30 20:43:04 +0300

    adding codes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
 import Course from "./Course";
+import { useState } from "react";
+
+function getRandomCourse() {
+  const courseArray = ["Angular, Bootstrap, Ccsharp, Web"];
+  return courseArray[Math.floor(Math.random() * courseArray.length)];
+}
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  const courseList = courses.map((course, index) => {
+    return <Course key={index} courseName={course} />;
+  });
+
+  const handleClick = () => {
+    setCourses([...courses, getRandomCourse()]); //keep last courses
+  };
+
   return (
     <div className="App">
-      <Course />
+      <button onClick={handleClick}>Add Course</button>
+      <div>{courseList}</div>
     </div>
   );
 }
--- src/Course.js
+++ src/Course.js
@@ -3,8 +3,12 @@ import Bootstrap from "./images/bootstrap5.png";
 import Ccsharp from "./images/ccsharp.png";
 import Web from "./images/web.jpg";
 
-function Course() {
-  return <div>asdasdasd</div>;
+function Course(courseName) {
+  return (
+    <div>
+      <img src={Angular} alt="" />
+    </div>
+  );
 }
 
 export default Course;
